41732e6c45e402c2ff89338c56bf59d4d002f65f
Senpai <raohuraira331.rb@gmail.com>
2023-05-07 22:19:57 +0500

    bugs fixed

--- lib/pages/todo/todo_page.dart
+++ lib/pages/todo/todo_page.dart
@@ -43,8 +43,11 @@ class _TodoListState extends State<TodoList>
   }
 
   Future<void> deleteTodo(int id) async {
-    bool confirmDel =
-        await confirmDialogue(context: context, message: "Delete this todo?");
+    bool confirmDel = await confirmDialogue(
+      context: context,
+      message: "Delete this todo?",
+      title: "Delete Todo",
+    );
     if (confirmDel) {
       await NotificationService.cancelScheduleNotification(id: id);
       await _database.deleteTodo(id: id);
@@ -80,51 +83,84 @@ class _TodoListState extends State<TodoList>
     );
   }
 
-  void sortTodosAsComplete(List<Todo> todos) {
-    todos.sort(
-      (a, b) => a.complete.compareTo(b.complete),
-    );
+  List<Todo> filterTodosAsComplete(List<Todo> todos, int complete) {
+    if (complete == 0) {
+      return todos.where((todo) => todo.complete == 0).toList();
+    }
+    return todos.where((todo) => todo.complete == 1).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.only(
-          top: 10.0,
-          left: 10.0,
-          right: 10.0,
+      body: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(
+          parent: BouncingScrollPhysics(),
         ),
-        decoration: null,
-        child: StreamBuilder(
-          stream: _database.allTodos,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done ||
-                snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.grey,
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.only(
+            top: 10.0,
+            left: 10.0,
+            right: 10.0,
+          ),
+          decoration: null,
+          child: StreamBuilder(
+            stream: _database.allTodos,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done ||
+                  snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                );
+              }
+              final todos = snapshot.data!;
+              if (todos.isEmpty) {
+                return emptyWidget(
+                  icon: Icons.checklist_outlined,
+                  message: "Empty Todos",
+                );
+              }
+              final notComplete = filterTodosAsComplete(todos, 0);
+              final complete = filterTodosAsComplete(todos, 1);
+              return Container(
+                decoration: null,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    if (notComplete.isNotEmpty)
+                      myHeading("Todos ${notComplete.length}"),
+                    myListBuilder(notComplete),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    if (complete.isNotEmpty)
+                      myHeading("Completed ${complete.length}"),
+                    myListBuilder(complete),
+                  ],
                 ),
               );
-            }
-            final todos = snapshot.data!;
-            if (todos.isEmpty) {
-              return emptyWidget(
-                icon: Icons.checklist_outlined,
-                message: "Empty Todos",
-              );
-            }
-            sortTodosAsComplete(todos);
-            return Container(
-              decoration: null,
-              height: double.infinity,
-              width: double.infinity,
-              child: myListBuilder(todos),
-            );
-          },
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container myHeading(String message) {
+    return Container(
+      margin: const EdgeInsets.all(7.0),
+      child: Text(
+        message,
+        style: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
@@ -133,13 +169,12 @@ class _TodoListState extends State<TodoList>
   ListView myListBuilder(List<Todo> todos) {
     return ListView.builder(
       shrinkWrap: true,
-      physics: const AlwaysScrollableScrollPhysics(
-        parent: BouncingScrollPhysics(),
-      ),
+      physics: const NeverScrollableScrollPhysics(),
       itemCount: todos.length,
       itemBuilder: (context, index) {
         final todo = todos[index];
         final timeSchedule = getFormattedTime(todo.date);
+        bool isChecked = todo.complete != 0;
         return FadeTransition(
           opacity: _animation,
           child: SlideFromBottomTransition(
@@ -147,7 +182,7 @@ class _TodoListState extends State<TodoList>
             child: Container(
               margin: const EdgeInsets.symmetric(vertical: 5.0),
               decoration: BoxDecoration(
-                color: todo.complete == 1
+                color: isChecked
                     ? Colors.black.withAlpha(100)
                     : Colors.black.withAlpha(180),
                 borderRadius: BorderRadius.circular(10.0),
@@ -156,40 +191,63 @@ class _TodoListState extends State<TodoList>
                 key: ValueKey(todo.id!),
                 onLongPress: () async => await deleteTodo(todo.id!),
                 onTap: () => _showAddTodoBottomSheet(todo),
-                leading: Checkbox(
-                  value: todo.complete != 0,
-                  onChanged: (value) => handleCheckBox(todo),
+                leading: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  width: 24.0,
+                  height: 24.0,
+                  child: InkWell(
+                    onTap: () => handleCheckBox(todo),
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7.0),
+                        shape: BoxShape.rectangle,
+                        color:
+                            isChecked ? Colors.blueAccent : Colors.transparent,
+                        border: Border.all(
+                          color: isChecked ? Colors.blueAccent : Colors.white,
+                        ),
+                      ),
+                      child: isChecked
+                          ? const Center(
+                              child: Icon(
+                                Icons.check,
+                                size: 16.0,
+                                color: Colors.white,
+                              ),
+                            )
+                          : null,
+                    ),
+                  ),
                 ),
                 title: Text(
                   todo.text,
                   softWrap: true,
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
-                    color: todo.complete == 1 ? Colors.grey : Colors.grey[300],
+                    color: isChecked ? Colors.grey : Colors.grey[300],
                     fontSize: 16.0,
                     fontWeight: FontWeight.bold,
                     decorationThickness: 2.0,
-                    decoration:
-                        todo.complete == 1 ? TextDecoration.lineThrough : null,
+                    decoration: isChecked ? TextDecoration.lineThrough : null,
                   ),
                 ),
                 subtitle: Text(
                   '${todo.reminder == 1 ? "Passed: " : "Reminder: "}${timeSchedule ?? "Not Set"}',
                   style: TextStyle(
                     fontSize: 12.0,
-                    color: todo.reminder == 1 && todo.complete == 0
+                    color: todo.reminder == 1 && !isChecked
                         ? Colors.redAccent
                         : Colors.grey,
                   ),
                 ),
-                trailing: (todo.date != null &&
-                        todo.complete != 1 &&
-                        todo.reminder != 1)
-                    ? const Icon(
-                        Icons.alarm_on_sharp,
-                        color: Colors.lightBlue,
-                      )
-                    : null,
+                trailing:
+                    (todo.date != null && !isChecked && todo.reminder != 1)
+                        ? const Icon(
+                            Icons.alarm_on_sharp,
+                            color: Colors.lightBlue,
+                          )
+                        : null,
               ),
             ),
           ),
