8d68e5424d8687b4813fd546abdb3ae3ae149bd5
Senpai <raohuraira331.rb@gmail.com>
2023-05-14 03:59:58 +0500

    bugs fixed

--- lib/pages/profile/login_screen.dart
+++ lib/pages/profile/login_screen.dart
@@ -60,119 +60,112 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
           width: double.infinity,
           height: double.infinity,
-          constraints: const BoxConstraints.expand(height: double.infinity),
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics(),
-            ),
-            child: Form(
-              key: _formKey,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Center(
-                      child: CircleAvatar(
-                        // backgroundColor: Colors.transparent,
-                        // backgroundImage: AssetImage("assets/prof.png"),
-                        backgroundColor: Colors.lightBlue.withAlpha(120),
-                        radius: 80.0,
-                        child: const Icon(
-                          Icons.person,
-                          color: Colors.white,
-                          size: 100.0,
-                        ),
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: CircleAvatar(
+                      // backgroundColor: Colors.transparent,
+                      // backgroundImage: AssetImage("assets/prof.png"),
+                      backgroundColor: Colors.lightBlue.withAlpha(120),
+                      radius: 80.0,
+                      child: const Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 100.0,
                       ),
                     ),
-                    const SizedBox(
-                      height: 40.0,
-                    ),
-                    TextFormField(
-                      enabled: !_isLogin,
-                      controller: _email,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      keyboardType: null,
-                      decoration:
-                          decorationFormField(Icons.email, "Enter Email"),
-                      cursorColor: Colors.amber[200],
-                      style: const TextStyle(color: Colors.white),
-                      validator: textValidate,
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    TextFormField(
-                      enabled: !_isLogin,
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration:
-                          decorationFormField(Icons.password, "Enter password"),
-                      cursorColor: Colors.amber[200],
-                      style: const TextStyle(color: Colors.white),
-                      validator: textValidate,
-                    ),
-                    const SizedBox(
-                      height: 5.0,
-                    ),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        ElevatedButton(
-                          style: ButtonStyle(
-                            padding:
-                                MaterialStateProperty.all<EdgeInsetsGeometry>(
-                              const EdgeInsets.symmetric(
-                                vertical: 12.0,
-                                horizontal: 24.0,
-                              ),
-                            ),
-                            backgroundColor: MaterialStateColor.resolveWith(
-                              (states) => Colors.lightBlue.withAlpha(200),
-                            ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
+                  ),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
+                  TextFormField(
+                    enabled: !_isLogin,
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: null,
+                    decoration: decorationFormField(Icons.email, "Enter Email"),
+                    cursorColor: Colors.amber[200],
+                    style: const TextStyle(color: Colors.white),
+                    validator: textValidate,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  TextFormField(
+                    enabled: !_isLogin,
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration:
+                        decorationFormField(Icons.password, "Enter password"),
+                    cursorColor: Colors.amber[200],
+                    style: const TextStyle(color: Colors.white),
+                    validator: textValidate,
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          padding:
+                              MaterialStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.symmetric(
+                              vertical: 12.0,
+                              horizontal: 24.0,
                             ),
                           ),
-                          onPressed: () {},
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
+                          backgroundColor: MaterialStateColor.resolveWith(
+                            (states) => Colors.lightBlue.withAlpha(200),
+                          ),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16.0),
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                    TextButton(
-                      // onPressed: _isLogin
-                      //     ? null
-                      //     : () {
-                      //         Navigator.of(context)
-                      //             .pushReplacementNamed(registerRoute);
-                      //       },
-                      onPressed: () {},
-                      child: Text(
-                        "Click here to register",
-                        style: TextStyle(
-                          color: _isLogin ? Colors.grey : Colors.white,
-                          fontSize: 13.0,
+                        onPressed: () {},
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                  TextButton(
+                    // onPressed: _isLogin
+                    //     ? null
+                    //     : () {
+                    //         Navigator.of(context)
+                    //             .pushReplacementNamed(registerRoute);
+                    //       },
+                    onPressed: () {},
+                    child: Text(
+                      "Click here to register",
+                      style: TextStyle(
+                        color: _isLogin ? Colors.grey : Colors.white,
+                        fontSize: 13.0,
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
