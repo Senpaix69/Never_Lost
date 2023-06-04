9fd18d714cc7636e6b9f20c94e45719454ed833a
Senpai <raohuraira331.rb@gmail.com>
2023-06-05 00:46:19 +0500

    bugs fixed

--- lib/pages/profile/setting_screens/about_screen.dart
+++ lib/pages/profile/setting_screens/about_screen.dart
@@ -7,78 +7,79 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.only(
-            top: 50,
-            bottom: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back_ios_new_outlined),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Text(
-                    "About",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.left,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30.0),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
                   children: <Widget>[
-                    textMessageBold(
-                      message: appName,
-                      size: 18.0,
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_new_outlined),
+                      onPressed: () => Navigator.of(context).pop(),
                     ),
-                    textMessageBold(
-                      message: "Version: $version",
-                      size: 14.0,
+                    const SizedBox(
+                      width: 10,
                     ),
-                    const SizedBox(height: 10.0),
-                    const Text(aboutApp),
-                    textMessageBold(
-                      message: "Features:",
-                      size: 18.0,
+                    const Text(
+                      "About",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.left,
                     ),
-                    const SizedBox(height: 14.0),
-                    textMessageBold(
-                      message: "- TimeTable:",
-                      size: 18.0,
-                    ),
-                    const Text(aboutTimeTables),
-                    const SizedBox(height: 12.0),
-                    textMessageBold(
-                      message: "- Notes:",
-                      size: 18.0,
-                    ),
-                    const Text(aboutNotes),
-                    const SizedBox(height: 12.0),
-                    textMessageBold(
-                      message: "- Todos:",
-                      size: 18.0,
-                    ),
-                    const Text(aboutTodos),
-                    const SizedBox(height: 16.0),
-                    const Text(aboutMore),
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(height: 30.0),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      textMessageBold(
+                        message: appName,
+                        size: 18.0,
+                      ),
+                      textMessageBold(
+                        message: "Version: $version",
+                        size: 14.0,
+                      ),
+                      const SizedBox(height: 10.0),
+                      const Text(aboutApp),
+                      textMessageBold(
+                        message: "Features:",
+                        size: 18.0,
+                      ),
+                      const SizedBox(height: 14.0),
+                      textMessageBold(
+                        message: "- TimeTable:",
+                        size: 18.0,
+                      ),
+                      const Text(aboutTimeTables),
+                      const SizedBox(height: 12.0),
+                      textMessageBold(
+                        message: "- Notes:",
+                        size: 18.0,
+                      ),
+                      const Text(aboutNotes),
+                      const SizedBox(height: 12.0),
+                      textMessageBold(
+                        message: "- Todos:",
+                        size: 18.0,
+                      ),
+                      const Text(aboutTodos),
+                      const SizedBox(height: 16.0),
+                      const Text(aboutMore),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
